8f03c3033ccbd4f3375a0dd9ace60ccbf32feb4e
Mushfiqur Anik <43996786+mushfiqur-anik@users.noreply.github.com>
2021-01-31 21:00:38 -0500

    Added Mock Server

--- src/App.js
+++ src/App.js
@@ -1,35 +1,33 @@
-import {useState} from "react"
+import {useState, useEffect} from "react"
 import Header from     "./components/Header"
 import Tasks  from     "./components/Tasks"
 import AddTask from    "./components/AddTask"
 
 function App() {
-
   // Show Add task area 
   const [showAddTask, setShowAddTask] = useState(false)
 
   // Using state to set tasks
-  const [tasks, setTasks] = useState([
-    { 
-      id:       1,
-      text:     'Do Laundry',     
-      day:      'Feb 2nd at 10pm',
-      reminder: true
-    },
-    { 
-      id:       2,
-      text:     'Pay rent',     
-      day:      'Feb 3nd at 10am',
-      reminder: true
-    },
-    {
-      id:       3,
-      text:     'Do Exercise',     
-      day:      'Feb 2nd at 11am',
-      reminder: false
+  const [tasks, setTasks] = useState([])
+
+  useEffect(() => {
+    const getTasks = async() => {
+      const tasksFromServer = await fetchTasks()
+      setTasks(tasksFromServer)
     }
-  ])
+
+    getTasks()
+  }, [])
   
+  // Fetch all the tasks from server
+  const fetchTasks = async () => {
+    const res = await fetch("http://localhost:5000/tasks")
+    const data = await res.json()
+
+    console.log(data)
+    return data
+  }
+
   // Add new Task
   const addTask = (task) => {
     const id = Math.floor(Math.random() * 10000) + 1;
@@ -55,7 +53,7 @@ function App() {
   return (
     <div className="container">
       <Header onAdd={() => setShowAddTask(!showAddTask)}
-              showAdd={showAddTask}
+              showAdd={showAddTask }
               title="Task Tracker"/>     
       {showAddTask &&  <AddTask onAdd={addTask} />}
       {tasks.length > 0 ? 
